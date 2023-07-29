77c21a52305ef7f8347080f8acbb32c4a9c1dca6
kmch <kajetan_ch@o2.pl>
2023-07-29 14:22:49 +0200

    Rename to jupconfig

---
+++ jupconfig/jupyter.py
@@ -0,0 +1,41 @@
+# import os
+# import loggers
+# %load {os.path.dirname(loggers.__file__)}/jupyter.py
+
+# Set aliases of frequently used jupyter magic commands
+%alias_magic mi matplotlib -p inline
+%alias_magic mn matplotlib -p notebook
+
+# Set matplotlib backend (comment one or the other):
+# - non-interactive plots displayed in a notebook cell
+%matplotlib inline
+# - interactive plots displayed in a notebook cell
+# %matplotlib notebook (interactive version)
+
+# Set matplotlib style
+import matplotlib.pyplot as plt
+plt.style.reload_library()
+# print(plt.style.available) 
+# combine styles (right overwrites left wherever they overlap):
+plt.style.use(['default']) #, 'ggplot'])
+# plt.style.use(['default'])
+
+# Configure logging -------------------------------------------
+
+# 1. Set up loggers, handlers and load the log_lvl function
+from loggers.logs import *
+
+# 2. Set level of log-messages
+log_lvl(ERROR) # you can also use lll() alias
+# Other options in order of increasing importance and 
+# decreasing verbosity:
+# TRACE, DEBUG, INFO, WARNING, ERROR, CRITICAL
+# or using integers: 0, 10, 20, 30, 40, 50, respectively
+
+# Autocompleting ----------------------------------------------
+# (not sure if that works)
+#%config IPCompleter.greedy=True 
+
+# Automatically reload modules before execution
+%load_ext autoreload
+%autoreload 2
